325d9350deb349d2a8ec56c04ae8f19f2014f8bd
enowjohnenowbi <johnwashinton88@gmail.com>
2025-01-24 17:55:31 +0100

    commented out middleware

--- server.js
+++ server.js
@@ -21,26 +21,25 @@ initializeSocket(httpServer);
 
 // CORS configuration
 const corsOptions = {
-  origin: process.env.FRONT_END_URL,
-  // function(origin, callback) {
-  //   console.log({ origin, url:  process.env.FRONT_END_URL, });
+  origin: function(origin, callback) {
+    console.log({ origin, url:  process.env.FRONT_END_URL, });
 
 
-  //   const allowedOrigins = [
-  //     // 'http://localhost:3002',
-  //     // 'http://localhost:5173',
-  //     // 'https://taskmaster-app.onrender.com',
-  //     // 'https://coding-tasks-api.onrender.com',
-  //     process.env.FRONT_END_URL,
-  //   ];
-  //   // Allow requests with no origin (like mobile apps or curl requests)
-  //   if (!origin) return callback(null, true);
-  //   if (allowedOrigins.indexOf(origin) !== -1) {
-  //     callback(null, true);
-  //   } else {
-  //     callback(new Error('Not allowed by CORS'));
-  //   }
-  // },
+    const allowedOrigins = [
+      // 'http://localhost:3002',
+      // 'http://localhost:5173',
+      // 'https://taskmaster-app.onrender.com',
+      // 'https://coding-tasks-api.onrender.com',
+      process.env.FRONT_END_URL,
+    ];
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
@@ -48,7 +47,7 @@ const corsOptions = {
 };
 
 // Middleware
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
