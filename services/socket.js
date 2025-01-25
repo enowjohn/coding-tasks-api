d0ad8ef7c566b6eb4dbcf9c525061ce5a51dd2e0
enowjohnenowbi <johnwashinton88@gmail.com>
2025-01-25 09:25:47 +0100

    error fixed

--- services/socket.js
+++ services/socket.js
@@ -4,7 +4,7 @@ let io;
 const initializeSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: process.env.FRONTEND_URL || 'http://localhost:3002',
+      origin: process.env.FRONTEND_URL || 'http://localhost:3001',
       methods: ['GET', 'POST']
     }
   });
